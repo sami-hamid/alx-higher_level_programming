c0f6d8f2058882a0095e57b53395440d7de2075d
sami-hamid <samifadlallah21@gmail.com>
2023-12-01 22:37:09 -0800

    modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
- if __name__ "__main__": 
+ if __name__ == "__main__": 
 import sys 
 from calculator_1 import add, sub, mul, div 
 if len(sys.argv) != 4: 
@@ -9,13 +9,13 @@ a = int(sys.argv[1])
 b = int(sys.argv[3]) 
 op sys.argv[2] 
 if op == " + " : 
-print ("{} + {} = {}").format(a, b, add(a, b))) 
+print ("({} + {} = {}").format(a, b, add(a, b))) 
 elif op == "-" :
-print("{}-{}={}").format(a, b, sub(a, b))) 
+print("({} - {} = {}").format(a, b, sub(a, b))) 
 elif op == " *": 
-print("({}*{}={}")).format(a, b, mul(a, b)))
- elif op==" / ": 
-print("{}/{} = {}").format(a, b, div(a, b))) 
+print("({} * {} = {}").format(a, b, mul(a, b)))
+ elif op== " / ": 
+print("({} / {} = {}").format(a, b, div(a, b))) 
 else: 
 print("Unknown operator. Available operators:  +, -, *, and / “)
 exit(1)
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-if __name__=="__main__":
-    print """ all hidden directories """
+if __name__ == "__main__":
+     """ print all hidden directories """
     import hidden_4
 
     for i in dir(hidden_4):
